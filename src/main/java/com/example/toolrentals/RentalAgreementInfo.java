c881e408599587019597968ad7c49fe5cce1107a
Pete <peter_izzo@yahoo.com>
2024-08-12 11:47:49 -0700

    Initial commit.  Moved code from Tool-Sales

---
+++ src/main/java/com/example/toolrentals/RentalAgreementInfo.java
@@ -0,0 +1,100 @@
+package com.example.toolrentals;
+
+import java.time.LocalDate;
+
+// Rental Agreement Info
+public class RentalAgreementInfo {
+
+    private String itemId;
+    private String custId;
+    private String itemToolType;
+    private String itemToolBrand;
+    private String customerName;
+    private Float dailyRentalFee;
+    private boolean weekdayCharge;
+    private boolean weekendCharge;
+    private boolean holidayCharge;
+    private LocalDate startDate;
+    private int rentalDays;
+    private int chargeDays;
+    private Float totalCharge;
+    private Float discountPct;
+    private Float discountAmt;
+    private Float afterDiscountTotal;
+
+    public RentalAgreementInfo( ) {
+            }
+
+    public String getItemId() {
+        return itemId;
+    }
+    public String getCustId() {
+        return custId;
+    }
+    public String getItemToolType() {
+    return itemToolType;
+}
+    public String getItemToolBrand() {
+        return itemToolBrand;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public Float getDailyRentalFee() { return dailyRentalFee; }
+    public boolean isWeekdayCharge() { return weekdayCharge; }
+    public boolean isWeekendCharge() {
+        return weekendCharge;
+    }
+    public boolean isHolidayCharge() {
+        return holidayCharge;
+    }
+    public LocalDate getStartDate() { return startDate; }
+    public int  getRentalDays() {
+        return rentalDays;
+    }
+    public int getChargeDays() {
+        return chargeDays;
+    }
+    public Float getTotalCharge() {
+        return totalCharge;
+    }
+    public Float getDiscountPct() {
+        return discountPct;
+    }
+    public Float getDiscountAmt() {
+        return discountAmt;
+    }
+    public Float getAfterDiscountTotal() {
+        return afterDiscountTotal;
+    }
+
+    public void setItemId(String itemId) { this.itemId = itemId;}
+    public void setCustId(String custId){ this.custId = custId;}
+    public void setItemToolType(String itemToolType){ this.itemToolType = itemToolType;}
+    public void setItemToolBrand(String itemToolBrand){ this.itemToolBrand = itemToolBrand;}
+    public void setCustomerName(String customerName){ this.customerName = customerName;}
+    public void setDailyRentalFee(Float dailyRentalFee){ this.dailyRentalFee = dailyRentalFee;}
+    public void setWeekdayCharge(boolean weekdayCharge){ this.weekdayCharge = weekdayCharge;}
+    public void setWeekendCharge(boolean weekendCharge){ this.weekendCharge = weekendCharge;}
+    public void setHolidayCharge(boolean holidayCharge){ this.holidayCharge = holidayCharge;}
+    public void setStartDate(LocalDate startDate){ this.startDate = startDate;}
+    public void setRentalDays(int rentalDays){ this.rentalDays = rentalDays;}
+    public void setChargeDays(int chargeDays){ this.chargeDays = chargeDays;}
+    public void setTotalCharge(Float totalCharge){ this.totalCharge = totalCharge;}
+    public void setDiscountPct(Float discountPct){ this.discountPct = discountPct;}
+    public void setDiscountAmt(Float discountAmt){ this.discountAmt = discountAmt;}
+    public void setAfterDiscountTotal(Float afterDiscountTotal){ this.afterDiscountTotal = afterDiscountTotal;}
+
+
+
+}
+
+
+
+
+//    @Override
+//        public String toString() {
+//            return name;
+//        }
+
+ //   }
---
+++ src/main/java/com/example/toolrentals/RentalCalculations.java
@@ -0,0 +1,51 @@
+package com.example.toolrentals;
+
+import java.time.LocalDate;
+
+public class RentalCalculations {
+
+
+    public RentalCalculations(RentalAgreementInfo rentalAgreementInfo){
+
+        LocalDate endDate = rentalAgreementInfo.getStartDate();
+        endDate = endDate.plusDays((long) rentalAgreementInfo.getRentalDays() - 1);
+        Float invoiceDailyCharge = 0.00F;
+        Float totalCharge = 0.00F; // invoice total
+        Float afterDiscountTotal = 0.00F;
+        int chargeDays = 0;
+
+
+        LocalDate currentDate = rentalAgreementInfo.getStartDate();
+        while(!currentDate.isAfter(endDate)){
+
+            invoiceDailyCharge = 0F;
+
+            if (rentalAgreementInfo.isHolidayCharge() && ToolRentalSystem.OrderDetails.isHoliday(currentDate)){
+                invoiceDailyCharge = rentalAgreementInfo.getDailyRentalFee();
+                chargeDays +=1;
+            }
+            if (!ToolRentalSystem.OrderDetails.isHoliday(currentDate)) {
+                if (rentalAgreementInfo.isWeekdayCharge() && ToolRentalSystem.OrderDetails.isWeekday(currentDate)) {
+                    invoiceDailyCharge = rentalAgreementInfo.getDailyRentalFee();
+                    chargeDays += 1;
+                } else if (rentalAgreementInfo.isWeekendCharge() && !ToolRentalSystem.OrderDetails.isWeekday(currentDate)) {
+                    invoiceDailyCharge = rentalAgreementInfo.getDailyRentalFee();
+                    chargeDays += 1;
+                } else {
+                    invoiceDailyCharge = 0F;
+                }
+            }
+            totalCharge += invoiceDailyCharge;
+
+            currentDate = currentDate.plusDays(1);
+        }
+
+        afterDiscountTotal = (float) (totalCharge - (totalCharge * rentalAgreementInfo.getDiscountPct()/100));
+        rentalAgreementInfo.setAfterDiscountTotal(afterDiscountTotal);
+        rentalAgreementInfo.setTotalCharge(totalCharge);
+        rentalAgreementInfo.setChargeDays(chargeDays);
+        rentalAgreementInfo.setDiscountAmt(totalCharge * rentalAgreementInfo.getDiscountPct()/100);
+
+
+    }
+}
